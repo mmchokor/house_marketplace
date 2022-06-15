60a7d2554455b17d41be5962aa8bc10a0c0d7bde
mmchokor <mohameddd701@gmail.com>
2022-06-16 02:04:44 +0300

    16. Single Listings, Map, Slider & Edit - 3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import SignIn from './pages/Signin'
 import SignUp from './pages/Signup'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
    return (
@@ -27,6 +29,11 @@ function App() {
                <Route path='/sign-up' element={<SignUp />} />
                <Route path='/forgot-password' element={<ForgotPassword />} />
                <Route path='/create-listing' element={<CreateListing />} />
+               <Route
+                  path='/category/:categoryName/:listingId'
+                  element={<Listing />}
+               />
+               <Route path='/contact/:landlordId' element={<Contact />} />
             </Routes>
             <Navbar />
          </Router>
